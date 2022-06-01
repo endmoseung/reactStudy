a27562b689859d10b2b521b03571ae30e6409a41
moseung <tmdah900@naver.com>
2022-06-01 20:10:31 +0900

    init

--- habit-trackers/src/components/habit.jsx
+++ habit-trackers/src/components/habit.jsx
@@ -1,13 +1,13 @@
 import React, { PureComponent } from 'react';
 
 
-class Habit extends PureComponent {
+class Habit extends PureComponent { // PureComponent : 최상위에 있는 데이터가 변하지 않으면 render 함수가 호출되지 않는다. 일반 component와 다른점은 shouldComponentUpdate()의 유무인데 이함수는 간단하게 말하면 이전상태의 props나 state를 현재의 propse나 state와 비교하여 변경점이 없으면 false를 리턴해서 변경점이 없게하고 업데이트가 필요하면 true를 리턴하여 원래 component처럼 업뎃한다, 이것은 shallow하게 비교하는데 이떄 shallow하다는것은 비교하는 객체가 있다고하면 ref와 비교하여 즉 최상위의 값이 같고 자식이 변경되어도 같다고 취급하고 false를 return deepcomparison은 shallow와 반대로 자식요소에 변경점이 있다면 변경해야 한다고 간주한다.
 
   componentDidMount(){
-    console.log(`habit: ${this.props.habit.name} mounted`);
+    console.log(`habit: ${this.props.habit.name} mounted`); //component가 render된 이후에 실행
 }
 componentWillUnmount(){
-  console.log(`habit: ${this.props.habit.name} will unmount`);
+  console.log(`habit: ${this.props.habit.name} will unmount`); //component가 사라질떄 즉 버튼을 누르자마자 호출
 }
   handleIncrement = ()=>{
     this.props.onIncrement(this.props.habit);
