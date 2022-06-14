ab4e88b289074ebfa8031044b60364fd89924e0e
moseung <tmdah900@naver.com>
2022-06-14 16:30:06 +0900

    add loading bar ad imagefileinput and change color

--- cardMaker/src/components/image_file_input/image_file_input.jsx
+++ cardMaker/src/components/image_file_input/image_file_input.jsx
@@ -1,16 +1,18 @@
 import React, { useRef } from "react";
+import { useState } from "react";
 import styles from "./image_file_input.module.css";
 
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
+  const [loading, setLoading] = useState(false);
   const inputRef = useRef();
   const onButtonClick = () => {
     inputRef.current.click(); // 라벨이 클릭돼도 인풋이 클릭된것 처럼
   };
 
   const FileChange = async (event) => {
-    console.log(event);
+    setLoading(true);
     const uploaded = await imageUploader.upload(event.target.files[0]); //이게 실행될때까지 기다렸다가 완료되면 uploaded에 할당
-    console.log(uploaded);
+    setLoading(false); //로딩이 끝났으면  다시 false로 바꿔준다
     onFileChange({
       name: uploaded.original_filename,
       url: uploaded.url,
@@ -18,14 +20,16 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
   };
   return (
     <div className={styles.container}>
-      <label
-        onClick={onButtonClick}
-        className={onFileChange.name ? styles.buttonActive : styles.button}
-        for="ex_file"
-        htmlFor=""
-      >
-        {name || "no file"}
-      </label>
+      {!loading && (
+        <label
+          onClick={onButtonClick}
+          className={name ? styles.buttonActive : styles.button}
+          for="ex_file"
+          htmlFor=""
+        >
+          {name || "no file"}
+        </label>
+      )}
       <input
         ref={inputRef}
         accept="image/*"
@@ -36,6 +40,7 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
         multiple
         onChange={FileChange}
       />
+      {loading && <div className={styles.loading}></div>}
     </div>
   );
 };
